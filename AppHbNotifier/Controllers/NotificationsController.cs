99c1ad2153abdf44a3cf293a8a3c8d0998d1112a
Paul Duran <paulduran@gmail.com>
2012-03-17 23:46:54 +1100

    initial commit

---
+++ AppHbNotifier/Controllers/NotificationsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AppHbNotifier.Models;
+
+namespace AppHbNotifier.Controllers
+{
+    public class NotificationsController : ApiController
+    {        
+        // POST /api/values
+        public HttpResponseMessage<Notification> Post(string id, Notification notification)
+        {
+            var response = new HttpResponseMessage<Notification>(notification, HttpStatusCode.Created);
+            return response;
+        }
+    }
+}
---
+++ AppHbNotifier/Models/Notification.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppHbNotifier.Models
+{
+    public class Notification
+    {
+        public Application Application { get; set; }
+        public Build Build { get; set; }
+    }
+    public class Application
+    {
+        public string Name { get; set; }
+    }
+    public class Build
+    {
+        public Commit Commit { get; set; }
+        public string Status { get; set; }
+    }
+    public class Commit
+    {
+        public string Id { get; set; }
+        public string Message { get; set; }
+    }
+}
